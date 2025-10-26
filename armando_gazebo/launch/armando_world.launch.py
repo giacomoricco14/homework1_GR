7480f900a09ffa0fca0d2b4345eb6d2010e338a3
Emanuele Fusco <emanuele.fusco2@studenti.unina.it>
2025-10-26 13:59:33 +0100

    HW1: refined

--- armando_gazebo/launch/armando_world.launch.py
+++ armando_gazebo/launch/armando_world.launch.py
@@ -1,88 +1,51 @@
-# Software License Agreement (BSD License 2.0)
-#
-# Copyright (c) 2023, Metro Robots
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Metro Robots nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# armando world launcher
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.conditions import IfCondition, UnlessCondition
 import os
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
-
- # Percorso del pacchetto
+    # Package path
     pkg_path = get_package_share_directory('armando_description')
-
-    # Percorsi dei file
-    # urdf_path = os.path.join(pkg_path, 'urdf', 'arm.urdf')
-    
-    #  # Leggi il file URDF
-    # with open(urdf_path, 'r') as infp:
-    #     robot_desc = infp.read()
         
+    # Armando xacro path
     xacro_armando = PathJoinSubstitution([pkg_path, "urdf", "armando.urdf.xacro"])
-    # Parametri del robot
-    # params = {'robot_description': robot_desc}
+    # Robot description given by the xacro command
     params = {'robot_description': Command(['xacro ', xacro_armando, ' j0:=2.0', ' j1:=0.2', ' j2:=0.0', ' j3:=0.0'])}
-    
-    # these are the arguments you can pass this launch file, for example paused:=true
+
+
+    #### Arguments ####
+
     controller_arg = DeclareLaunchArgument(
         name='ctrl',
         description = 'Select: 0 for pos controller; 1 for trajectory controller',
         default_value='0',
     )
     user = LaunchConfiguration("ctrl")
+
     gui_arg = DeclareLaunchArgument(
         name='gui',
         default_value='true',
     )
+
     package_arg = DeclareLaunchArgument('urdf_package',
                                         description='The package where the robot description is located',
                                         default_value='armando_description')
+    
     model_arg = DeclareLaunchArgument('urdf_package_path',
-                                      description='The path to the robot description relative to the package root',
-                                      default_value='urdf/armando.urdf.xacro') # punto 2c
+                                        description='The path to the robot description relative to the package root',
+                                        default_value='urdf/armando.urdf.xacro') # introduced from point 2c
 
+
+    #### Nodes ####
 
     empty_world_launch = IncludeLaunchDescription(
         PathJoinSubstitution([FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py']),
@@ -109,7 +72,7 @@ def generate_launch_description():
         output='screen',
     )
 
-    # Punto 2c:
+    # Nodes introduced starting from point 2c:
     joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
@@ -130,11 +93,11 @@ def generate_launch_description():
         condition=IfCondition(PythonExpression([user, ' == 1']))
     ) 
 
-    #Launch the ros2 controllers after the model spawns in Gazebo 
+    # Launch the ros2 controllers after the Robot construction in Gazebo 
     delay_joint_traj_controller = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=urdf_spawner_node, # come si accende questo nodo aspetta il timer e ...
-            on_exit=[position_controller, trajectory_controller], #... in uscita apri questo nodo
+            target_action=urdf_spawner_node, # Once this node is active, wait
+            on_exit=[position_controller, trajectory_controller], # On exit, start this nodes
         )
     )
 
@@ -152,10 +115,12 @@ def generate_launch_description():
             "/camera@sensor_msgs/msg/Image@gz.msgs.Image",
             "/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
             "--ros-args",
-            "-r", "/camera:=/videocamera"],
+            "-r", "/camera:=/videocamera"], # Remapping
         output="screen"
     )
 
+
+    # List of Arguments and Nodes
     return LaunchDescription([
         controller_arg,
         gui_arg,
